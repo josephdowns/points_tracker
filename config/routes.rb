e790a821ed59882b21b62f8d95e2c3c7a36cf826
Joseph Downs <jos.w.downs@gmail.com>
2022-09-29 16:26:15 -0600

    Create get method for points

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post "/user_points", to: 'user_points#create'
+      get "/user_points", to: 'user_points#index'
     end
   end
 end
--- spec/requests/api/v1/user_points/user_points_request_spec.rb
+++ spec/requests/api/v1/user_points/user_points_request_spec.rb
@@ -22,8 +22,28 @@ RSpec.describe "User Points API" do
 
     post "/api/v1/user_points", headers: headers, params: JSON.generate(points_params)
     create_points = UserPoint.last
+    created = JSON.parse(response.body, symbolize_names: true)[:data]
+    
+    expect(response).to be_successful
     expect(create_points.payer).to eq("Target")
     expect(create_points.points).to eq(460)
+    expect(created[:attributes][:payer]).to eq("Target")
+    expect(created[:attributes][:points]).to eq(460)
 
+
+  end
+
+  it "can list all payers and points" do
+    headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+
+    get "/api/v1/user_points", headers: headers
+    index = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(response).to be_successful
+    expect(index.keys.count).to eq(3)
+    expect(index[:Unilever]).to eq(200)
   end
 end
